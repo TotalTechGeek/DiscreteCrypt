654df9af755f92ba2b74c6be6c16c0259ec89add
Jesse Mitchell <TotalTechGeek@users.noreply.github.com>
2018-02-22 13:04:39 -0600

    Added Password Masking to Unix

--- tool/toolCrypto.h
+++ tool/toolCrypto.h
@@ -70,11 +70,54 @@ std::string getPassword()
     return result;
 }
 #else
+#include <termios.h>
+#include <unistd.h>
+int getch() 
+{
+    int ch;
+    struct termios t_old, t_new;
+
+    tcgetattr(STDIN_FILENO, &t_old);
+    t_new = t_old;
+    t_new.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &t_new);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &t_old);
+    return ch;
+}
+
+
 std::string getPassword()
 {
-    std::string result; 
-    getline(std::cin, result);
-    return result;
+    using namespace std;
+    const char BACKSPACE=127;
+    const char RETURN=10;
+
+    string password;
+    unsigned char ch=0;
+
+
+    while((ch=getch())!=RETURN)
+    {
+        if (ch==BACKSPACE)
+        {
+            if(password.length()!=0)
+            {
+                cout << "\b \b";
+                password.resize(password.length()-1);
+            }
+        }
+        else
+        {
+            password += ch;
+            cout << '*';
+        }
+    }
+
+    cout << endl;
+    return password;
 }
 #endif
 
